6a03e8f512f54e902028d51d68d4f915899ebdae
Wilson Coelho <wlfcoelho@gmail.com>
2025-06-03 07:33:01 -0300

    config rabbitmq enviado as msg

---
+++ src/main/java/com/coelhoworks/coelhosensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.coelhoworks.coelhosensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+  @Bean
+  public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+
+    return new RabbitAdmin(connectionFactory);
+  }
+
+  @Bean
+  public Queue queue(){
+    return QueueBuilder.durable("tenperature-monitoring.process-temperature.v1.q")
+            .build();
+  }
+
+
+  public FanoutExchange exchange(){
+    return ExchangeBuilder.fanoutExchange("tenperature-processsing.temperature-received.v1.e")
+            .build();
+  }
+
+  @Bean
+  public Binding binding(){
+    return BindingBuilder.bind(queue()).to(exchange());
+  }
+}
